0256c5e4b290ff11a698b568db48d6a4a4f1c005
Jordan <jordankbassett@gmail.com>
2020-05-27 20:56:27 -0500

    enjoy my confusion(trying to function out similar to bandsintown and activity 10

--- assets/omdb.js
+++ assets/omdb.js
@@ -11,19 +11,52 @@ $.ajax({
     method: "GET"
    
 }).then(function(response) {
-    //$("#movie-view").text(JSON.stringify(response));
+//("#movie-view").text(JSON.stringify(response));
     console.log(response)
 
-    var plot = $("<p>").text("Plot: " +response.Plot);
-    var poster = $("<img>")
-    poster.attr("src", response.Poster)
+  var plot = $("src").text("Plot: " +response.Plot);
+   var poster = $("<img>")
+   poster.attr("src", response.Poster)
     
-    poster.append(plot);  
+  poster.append(plot);  
     
     
-    $("#movie-view").append(poster);
+   $("#movie-view").append(poster);
+   var Plottwo = $("<h1>").text(response.Plot);
+
+
+
+
+   $()
+
+
 
     
    
 });
+// we want to display the information in this function under the poster.
 });
+    //  function displayMovieInfo() {
+    //     event.preventDefault();
+       
+    //     var movie = $(this).attr("data-name");
+    //     var queryURL = "https://www.omdbapi.com/?t=" + movie + "apikey=bb6e985a";
+    //     $.ajax({
+    //         url: queryURL,
+    //         method: "GET"
+    //       }).then(function(response) {
+    //         var movieDiv = $("<div class=movie-view'>");
+    
+    //         var rating = response.Rated;
+    //         var pOne = $("<p>").text("Rating: " + rating);
+    //         movieDiv.append(pOne);
+
+    //         var plot = response.Plot;
+    //         var pThree = $("<p>").text("Plot: " + plot);
+    //         movieDiv.append(pThree);
+          
+    //      $("#movies-view").prepend(movieDiv);
+    //     }); 
+    
+
+    // }
